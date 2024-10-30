7b26e276ea89dea5f1c13dda3e4ad83a021cfb8d
Talia Friedman <taliafriedman22@gmail.com>
2024-10-30 15:15:41 +0200

    added pinn option

--- index.js
+++ index.js
@@ -13,22 +13,26 @@ const DB = [
     {
         id: 0,
         text: "Enter text",
-        backgroundColor: "color1"
+        backgroundColor: "color1",
+        isPinned: false
     },
     {
         id: 1,
         text: "Enter text",
-        backgroundColor: "color1"
+        backgroundColor: "color1",
+        isPinned: false 
     },
     {
         id: 2,
         text: "Enter text",
-        backgroundColor: "color1"
+        backgroundColor: "color1",
+        isPinned: false
     },
     {
         id: 3,
         text: "Enter text",
-        backgroundColor: "color1"
+        backgroundColor: "color1",
+        isPinned: false
     }
 ];
 
@@ -42,13 +46,14 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    const { text, backgroundColor } = req.body;
+    const { text, backgroundColor, isPinned = false } = req.body;
     if (!text || !backgroundColor) 
         return res.status(400).send({ error: 'text and background are required' });
     const newCard = {
         id: getNextId(),
         text,
         backgroundColor,
+        isPinned,
     };
     DB.push(newCard);
     res.json(newCard);
